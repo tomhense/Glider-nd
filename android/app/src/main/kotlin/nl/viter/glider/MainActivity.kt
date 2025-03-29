c160f259e8ad464bdffd950bece23fe6a27e755e
cristibctr <cristian.bucataru@gmail.com>
2025-03-29 12:28:21 +0200

    feat: use 120hz

--- android/app/src/main/kotlin/nl/viter/glider/MainActivity.kt
+++ android/app/src/main/kotlin/nl/viter/glider/MainActivity.kt
@@ -1,5 +1,14 @@
 package nl.viter.glider
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.attributes.preferredRefreshRate = 120f
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+}
